74e4015a25c43327f790df61b2fd67f98e06b6d3
Mac <Mac>
2020-10-30 01:00:40 +0530

    SPM Package Created

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TrackableScrollView",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "TrackableScrollView",
+            targets: ["TrackableScrollView"]),
+    ],
+    targets: [
+        .target(
+            name: "TrackableScrollView",
+            dependencies: []),
+    ],
+    swiftLanguageVersions: [.v5]
+)
